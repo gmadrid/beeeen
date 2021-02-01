5a2c24be0c26a9a21013be79ff723120e0a14ecf
George Madrid <gmadrid@gmail.com>
2021-02-01 12:40:19 -0500

    Removing unwanted output from main.rs.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,8 @@ use std::fs::File;
 
 fn main() {
     let f = File::open("./ubuntu.torrent").unwrap();
-
     let mut reader = beeenn::BEReader::new(f);
     let value = reader.next_value().unwrap().unwrap();
-
-    println!("TOP: {:?}", value);
-
-    let value = value.get("announce-list").unwrap();
-
-    println!("VALUE: {:?}", value);
+    println!("{:?}", value);
 }
+
